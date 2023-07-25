c19702e9ea66e670824fffa3100a26a029c78a74
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-07-25 05:30:31 +0200

    added weather details

--- src/app/components/detailed-weather/detailed-weather.component.ts
+++ src/app/components/detailed-weather/detailed-weather.component.ts
@@ -8,5 +8,38 @@ import { WeatherService } from 'src/app/services/weather.service';
   styleUrls: ['./detailed-weather.component.scss']
 })
 export class DetailedWeatherComponent {
+  weatherDetails!: Weather;
   
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(){
+    this.getGeolocation();
+  }
+
+  getGeolocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          this.getWeatherDetailsByCoordinates(latitude, longitude);
+        },
+        (error) => {
+          console.error('Error getting geolocation: ', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not available in this browser.');
+    }
+  }
+
+  getWeatherDetailsByCoordinates(latitude: number, longitude: number): void{
+    this.weatherService.getWeatherByCoordinates(latitude, longitude).subscribe(
+      (weatherData: Weather) => {
+        this.weatherDetails = weatherData;
+    })
+  }
+
+
+
 }
